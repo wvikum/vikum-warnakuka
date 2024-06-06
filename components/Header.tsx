d11162f351a0957069ddcf06c75c3b93365bcaa2
vikum warnakula <vwarnakula@deputy.com>
2024-06-06 21:04:10 +1000

    bg color change

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Header.module.css";
 
 const profileData = {
   name: "Vikum Warnakula",
@@ -12,23 +12,39 @@ const profileData = {
     {
       href: "https://www.instagram.com/sky_mantis?igsh=c2l2a3RjZXh5aGxi&utm_source=qr",
       label: "SkyMantis",
-    }, // Add your Instagram link here
+    },
   ],
 };
 
 const Header: React.FC = () => {
   const { name, title, profileImgSrc, socialLinks } = profileData;
+  const [isHidden, setIsHidden] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setIsHidden(true);
+    } else {
+      setIsHidden(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const renderSocialLinks = () =>
     socialLinks.map((link, index) => (
-      <span key={index}>
+      <span key={index} className={styles.socialLinks}>
         <a href={link.href}>{link.label}</a>
         {index < socialLinks.length - 1 && " | "}
       </span>
     ));
 
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${isHidden ? styles.hidden : ""}`}>
       <Image
         src={profileImgSrc}
         alt="Profile Picture"
